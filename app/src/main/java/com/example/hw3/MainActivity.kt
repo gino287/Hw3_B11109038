35b5df7f591f89256367e06868ebd0cec0426893
gino287 <35049957a@gmail.com>
2024-06-12 03:20:24 +0800

    0612_Hw3_finish

--- app/src/main/java/com/example/hw3/MainActivity.kt
+++ app/src/main/java/com/example/hw3/MainActivity.kt
@@ -1,24 +1,14 @@
 package com.example.hw3
 
 import android.Manifest
-import android.app.Notification
-import android.app.NotificationChannel
-import android.app.NotificationManager
-import android.app.PendingIntent
-import android.content.ContentValues
-import android.content.Context
-import android.content.Intent
 import android.graphics.Bitmap
-import android.graphics.BitmapFactory
 import android.net.Uri
 import android.os.Bundle
-import android.provider.MediaStore
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
 import androidx.activity.result.contract.ActivityResultContracts
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
@@ -36,23 +26,27 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.asImageBitmap
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.work.Data
+import androidx.work.OneTimeWorkRequest
+import androidx.work.WorkManager
 import coil.compose.AsyncImage
 import com.example.hw3.ui.theme.Hw3Theme
-import java.io.InputStream
+import com.example.hw3.workers.ImageProcessingWorker
 
 class MainActivity : ComponentActivity() {
-    private val channel = NotificationChannel("Hw3", "Hw3", NotificationManager.IMPORTANCE_DEFAULT)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
+        //權限請求，用於請求通知權限
         val requestPermissionLauncher =
             registerForActivityResult(
                 ActivityResultContracts.RequestPermission()
             ) { isGranted: Boolean ->
                 if (!isGranted) {
+                    //如果權限未被授予，顯示提示訊息，並關閉應用
                     Toast.makeText(
                         this@MainActivity,
                         "需要權限才能使用此程式",
@@ -62,6 +56,7 @@ class MainActivity : ComponentActivity() {
                     this.finishAffinity()
                 }
             }
+        //發起權限請求
         requestPermissionLauncher.launch(
             Manifest.permission.POST_NOTIFICATIONS
         )
@@ -72,159 +67,79 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    ImagePicker()
+                    ImagePicker()//調用用於選擇和顯示圖片的組件
                 }
             }
         }
     }
 
 
-    //讀取圖片
+    //選擇圖片和處理圖片
     @Composable
     fun ImagePicker() {
+        //儲存選擇的圖片uri狀態
         var imageUri by remember { mutableStateOf<Uri?>(null) }
+        //儲存處理後圖片的點陣圖狀態
         var processedBitmap by remember { mutableStateOf<Bitmap?>(null) }
+        //圖片選擇
         val imagePickerLauncher = rememberLauncherForActivityResult(
             contract = ActivityResultContracts.GetContent()
         ) { uri: Uri? ->
             imageUri = uri
-            processedBitmap = null  // 选择新图片时，清除之前的处理结果
+            processedBitmap = null  // 選擇新圖片時，清理之前的處理結果
         }
 
         Box(modifier = Modifier.fillMaxSize()) {
 
-            // 使用 AsyncImage 显示选中的图片
-            if (processedBitmap != null) {
-                Image(
-                    bitmap = processedBitmap!!.asImageBitmap(),
-                    contentDescription = "Processed Image",
+            // 使用 AsyncImage 顯示選中的圖片
+            imageUri?.let { uri ->
+                AsyncImage(
+                    model = uri,
+                    contentDescription = "Selected Image",
                     modifier = Modifier
                         .fillMaxWidth()
                         .align(Alignment.TopCenter)
                 )
-            } else {
-                imageUri?.let { uri ->
-                    AsyncImage(
-                        model = uri,
-                        contentDescription = "Selected Image",
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .align(Alignment.TopCenter)
-                    )
-                }
             }
+            //底部按鈕行，有選擇圖片及處理圖片
             Row(
                 modifier = Modifier
                     .align(Alignment.BottomCenter)
                     .fillMaxWidth()
                     .padding(bottom = 30.dp),//增加底部距離
-                horizontalArrangement = Arrangement.SpaceBetween
+                horizontalArrangement = Arrangement.spacedBy(15.dp)
             ) {
-                // 按钮用于打开相册
-                Button(onClick = { imagePickerLauncher.launch("image/*") }) {
-                    Text("Select Image from Gallery")
+                // 打開相簿選擇圖片的按鈕
+                Button(
+                    modifier = Modifier.weight(1f),
+                    onClick = { imagePickerLauncher.launch("image/*") }) {
+                    Text(
+                        text = "選擇圖片",
+                        fontSize = 20.sp,
+                    )
                 }
 
-                // 添加一个按钮用于黑白化处理
-
-
-                val builder = Notification.Builder(this@MainActivity, "Hw3")
-                Button(onClick = {
-                    imageUri?.let { uri ->
-                        val inputStream: InputStream? =
-                            contentResolver.openInputStream(uri)
-                        val bitmap = BitmapFactory.decodeStream(inputStream)
-                        processedBitmap = convertToBlackAndWhite(bitmap)
-//                        // 你可以将 processedBitmap 显示在屏幕上，或者保存到文件系统
-//                        val imageIntentUri =
-//                            FileProvider.getUriForFile(
-//                                this@MainActivity,
-//                                this@MainActivity.applicationContext.packageName + ".provider",
-//                                saveBitmapToFile(this@MainActivity, processedBitmap!!)
-//                            )
-                        val imageIntentUri = saveImageToGallery(this@MainActivity,
-                            processedBitmap!!)
-                        val imageIntent = Intent(Intent.ACTION_VIEW)
-                        imageIntent.setDataAndType(imageIntentUri, "image/*")
-                        val pendingIntent = PendingIntent.getActivity(
-                            this@MainActivity,
-                            0,
-                            imageIntent,
-                            PendingIntent.FLAG_IMMUTABLE
-                        )
-                        imageIntent.setPackage("com.google.android.apps.photos")
-                        imageIntent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION)
-
-                        runOnUiThread {
-                            val notification: Notification =
-                                builder.setSmallIcon(R.drawable.ic_launcher_foreground)
-                                    .setContentTitle("Hw3")
-                                    .setContentText("圖片已處理好，點擊以開啟")
-                                    .setAutoCancel(true)
-                                    .setContentIntent(pendingIntent)
+                //處理圖片的按鈕，啟動後於後台workers處理工作
+                Button(
+                    modifier = Modifier.weight(1f),
+                    onClick = {
+                        imageUri?.let { uri ->
+                            val data: Data = Data.Builder().putString("image_uri", uri.toString())
+                                .build()
+                            val request =
+                                OneTimeWorkRequest.Builder(ImageProcessingWorker::class.java)
+                                    .setInputData(data)
                                     .build()
-
-
-                            val manager =
-                                getSystemService(NOTIFICATION_SERVICE) as NotificationManager
-                            manager.createNotificationChannel(channel)
-                            manager.notify(0, notification)
+                            WorkManager.getInstance(applicationContext).enqueue(request)
                         }
-                    }
-                }) {
-                    Text("Convert to Black and White")
+                    }) {
+                    Text(
+                        "進行黑白化處理",
+                        fontSize = 20.sp,
+                    )
                 }
             }
         }
-    }
-
-    //儲存圖片檔案
-
-    private fun saveImageToGallery(context: Context, bitmap: Bitmap,) :Uri {
-        val values = ContentValues().apply {
-            put(MediaStore.Images.Media.DISPLAY_NAME, "RRRRRRRR.jpg")
-            put(MediaStore.Images.Media.MIME_TYPE, "image/jpeg")
-            put(
-                MediaStore.Images.Media.RELATIVE_PATH,
-                "Pictures/${context.getString(R.string.app_name)}"
-            )
-            put(MediaStore.Images.Media.IS_PENDING, 1)
-        }
-
-        val resolver = context.contentResolver
-        val uri = resolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values)
-
-        uri?.let {
-            resolver.openOutputStream(it).use { outputStream ->
-                if (outputStream != null) {
-                    bitmap.compress(Bitmap.CompressFormat.JPEG, 100, outputStream)
-                }
-            }
-            values.clear()
-            values.put(MediaStore.Images.Media.IS_PENDING, 0)
-            resolver.update(it, values, null, null)
-        }
-
-        return uri!!
-    }
-
-    private fun convertToBlackAndWhite(bitmap: Bitmap): Bitmap {
-        val width = bitmap.width
-        val height = bitmap.height
-        val bwBitmap = Bitmap.createBitmap(width, height, Bitmap.Config.RGB_565)
-
-        for (x in 0 until width) {
-            for (y in 0 until height) {
-                val pixel = bitmap.getPixel(x, y)
-                val r = (pixel shr 16) and 0xff
-                val g = (pixel shr 8) and 0xff
-                val b = pixel and 0xff
-                val grayLevel = (0.299 * r + 0.587 * g + 0.114 * b).toInt()
-                val gray = (grayLevel shl 16) + (grayLevel shl 8) + grayLevel
-                bwBitmap.setPixel(x, y, gray)
-            }
-        }
-        return bwBitmap
     }
 
     @Preview(showBackground = true)
--- app/src/main/java/com/example/hw3/workers/ImageProcessingWorker.kt
+++ app/src/main/java/com/example/hw3/workers/ImageProcessingWorker.kt
@@ -1,51 +1,58 @@
 package com.example.hw3.workers
 
+import android.app.Activity
+import android.app.Notification
+import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.app.PendingIntent
+import android.content.ContentValues
 import android.content.Context
 import android.content.Intent
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import android.net.Uri
-import android.util.Log
+import android.provider.MediaStore
 import androidx.core.app.NotificationCompat
-import androidx.core.content.FileProvider
 import androidx.work.CoroutineWorker
 import androidx.work.WorkerParameters
 import com.example.hw3.R
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
-import java.io.File
-import java.io.FileOutputStream
+//創建通知頻道
+private val channel = NotificationChannel("Hw3", "Hw3", NotificationManager.IMPORTANCE_DEFAULT)
 
+//dowork方法執行後台任務
 class ImageProcessingWorker(
     context: Context,
     workerParams: WorkerParameters
 ) : CoroutineWorker(context, workerParams) {
     override suspend fun doWork(): Result = withContext(Dispatchers.IO) {
         try {
+            //從輸入數據獲取Uri
             val imageUri = inputData.getString("image_uri") ?: return@withContext Result.failure()
             val resolver = applicationContext.contentResolver
             resolver.openInputStream(Uri.parse(imageUri)).use { inputStream ->
                 val bitmap = BitmapFactory.decodeStream(inputStream)
+                //將圖片執行黑白化處理
                 val processedBitmap = convertToBlackAndWhite(bitmap)
-                val outputUri = saveBitmapToFile(processedBitmap, applicationContext)
+                //將處理後的圖片保存至圖庫
+                val outputUri = saveImageToGallery(applicationContext, processedBitmap)
+                //顯示處理完成的通知
                 showFinishedNotification(outputUri, applicationContext)
                 Result.success()
             }
         } catch (e: Exception) {
-            Log.e("ImageProcessingWorker", "Work failed", e)
             Result.failure()
         }
     }
 }
 
 
-//黑白化
+//黑白化處理
 fun convertToBlackAndWhite(bitmap: Bitmap): Bitmap {
     val width = bitmap.width
     val height = bitmap.height
-
+    //創建一個點陣圖
     val bwBitmap = Bitmap.createBitmap(width, height, Bitmap.Config.RGB_565)
 
     for (x in 0 until width) {
@@ -54,46 +61,77 @@ fun convertToBlackAndWhite(bitmap: Bitmap): Bitmap {
             val r = (pixel shr 16) and 0xff
             val g = (pixel shr 8) and 0xff
             val b = pixel and 0xff
+            //取出rgb的亮度套用轉灰階的公式
             val grayLevel = (0.299 * r + 0.587 * g + 0.114 * b).toInt()
             val gray = (grayLevel shl 16) + (grayLevel shl 8) + grayLevel
+            //設置每個像素點的灰度
             bwBitmap.setPixel(x, y, gray)
         }
     }
+    //回傳點陣圖
     return bwBitmap
 }
 
-
-private fun saveBitmapToFile(bitmap: Bitmap, context: Context): Uri {
-    val filename = "processed_image.png"
-    val outputDir = context.cacheDir
-    val outputFile = File(outputDir, filename)
-
-    FileOutputStream(outputFile).use { out ->
-        bitmap.compress(Bitmap.CompressFormat.PNG, 100, out)
+//保存圖片到圖庫
+private fun saveImageToGallery(context: Context, bitmap: Bitmap): Uri {
+    //使用ContentValues設置圖片屬性
+    val values = ContentValues().apply {
+        put(MediaStore.Images.Media.DISPLAY_NAME, "outputImage.jpg")
+        put(MediaStore.Images.Media.MIME_TYPE, "image/jpeg")
+        put(
+            MediaStore.Images.Media.RELATIVE_PATH,
+            "Pictures/${context.getString(R.string.app_name)}"
+        )
+        put(MediaStore.Images.Media.IS_PENDING, 1)
     }
-
-    return Uri.fromFile(outputFile)
+    //插入圖片到系統圖庫
+    val resolver = context.contentResolver
+    val uri = resolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values)
+    //保存圖片
+    uri?.let {
+        resolver.openOutputStream(it).use { outputStream ->
+            if (outputStream != null) {
+                bitmap.compress(Bitmap.CompressFormat.JPEG, 100, outputStream)
+            }
+        }
+        //更新圖片狀態
+        values.clear()
+        //更新圖庫中的圖片信息
+        values.put(MediaStore.Images.Media.IS_PENDING, 0)
+        resolver.update(it, values, null, null)
+    }
+    //返回保存的uri
+    return uri!!
 }
 
+//顯示處理完成後的通知
 private fun showFinishedNotification(uri: Uri, context: Context) {
-    val notificationManager = context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
-    val notificationId = 1
+    val builder = NotificationCompat.Builder(context, "Hw3")
+    val imageIntent = Intent(Intent.ACTION_VIEW)
+    imageIntent.setDataAndType(uri, "image/*")
+    //創建一個pendingIntent
+    val pendingIntent = PendingIntent.getActivity(
+        context,
+        0,
+        imageIntent,
+        PendingIntent.FLAG_IMMUTABLE
+    )
+    imageIntent.setPackage("com.google.android.apps.photos")
+    imageIntent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION)
+    //構件通知
+    val notification: Notification =
+        builder.setSmallIcon(R.drawable.ic_launcher_foreground)
+            .setContentTitle("圖片已處理好")
+            .setContentText("點擊以開啟")
+            .setAutoCancel(true)
+            .setContentIntent(pendingIntent)
+            .addAction(NotificationCompat.Action.Builder(0, "觀看圖片", pendingIntent).build())
+            .build()
 
-    val intent = Intent(Intent.ACTION_VIEW).apply {
-        data = uri
-        flags = Intent.FLAG_GRANT_READ_URI_PERMISSION
-    }
+    //獲取系統的manager服務
+    val manager =
+        context.getSystemService(Activity.NOTIFICATION_SERVICE) as NotificationManager
+    manager.createNotificationChannel(channel)
+    manager.notify(0, notification)
 
-    val pendingIntent = PendingIntent.getActivity(context, 0, intent, PendingIntent.FLAG_IMMUTABLE)
-
-    val notification = NotificationCompat.Builder(context, "channel_id")
-        .setContentTitle("Image Processing Complete")
-        .setContentText("Tap to view the processed image")
-        .setSmallIcon(R.drawable.ic_launcher_foreground)
-        .setContentIntent(pendingIntent)
-        .addAction(NotificationCompat.Action.Builder(0, "View Image", pendingIntent).build())
-        .setAutoCancel(true)
-        .build()
-
-    notificationManager.notify(notificationId, notification)
 }
